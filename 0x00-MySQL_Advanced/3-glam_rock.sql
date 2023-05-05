3386f0b71ae5975c1145d8ca4c892d7fe24e0f58
0xElectrifier-Ubuntu <samchijioke16@gmail.com>
2023-05-05 03:51:54 +0100

    Debugging checker error

--- 0x00-MySQL_Advanced/3-glam_rock.sql
+++ 0x00-MySQL_Advanced/3-glam_rock.sql
@@ -1,7 +1,7 @@
 -- Script that lists all bands with 'Glam rock' as their main style,
 -- ranked by their longevity
 --	Using 'metal_bands.sql.zip' dump file
-SELECT `band_name`, (IFNULL(`split`, '2023') - `formed`) AS `lifespan`
+SELECT `band_name`, (IFNULL(`split`, Year(Now())) - `formed`) AS `lifespan`
     FROM `metal_bands`
     WHERE FIND_IN_SET('Glam rock', IFNULL(`style`, '')) > 0
     ORDER BY `lifespan` DESC;
