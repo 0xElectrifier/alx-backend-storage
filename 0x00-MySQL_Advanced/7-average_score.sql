e984da33b4ebfb49e2e3fb2f36838a0694377566
0xElectrifier-Ubuntu <samchijioke16@gmail.com>
2023-05-07 02:23:17 +0100

    Setting a stored procedure to calculate average

--- 0x00-MySQL_Advanced/7-average_score.sql
+++ 0x00-MySQL_Advanced/7-average_score.sql
@@ -1,14 +1,19 @@
--- Script that creates a stored procedure ComputeAverageScoreForUser
--- that computes and store the average score for a student
-drop procedure IF EXISTS ComputeAverageScoreForUser;
-DELIMITER $$ ;
-CREATE PROCEDURE ComputeAverageScoreForUser(
-	IN user_id INT
-)
-BEGIN
-	UPDATE users
-   	SET average_score=(SELECT AVG(score) FROM corrections
-			     WHERE corrections.user_id=user_id)
-	WHERE id=user_id;
+-- Script that creates a stored procedure `ComputeAverageScoreForUser`
+-- that computes and store the average score for a student.
+DELIMITER //
 
-END;$$
+DROP PROCEDURE IF EXISTS ComputeAverageScoreForUser //
+
+CREATE PROCEDURE ComputeAverageScoreForUser (IN `user_id` INT)
+    BEGIN
+	SET @avg_score = (SELECT AVG(`score`)
+				FROM `corrections`
+				WHERE user_id = user_id);
+	IF EXISTS (SELECT `id` FROM `users` WHERE id = user_id) THEN
+	    UPDATE `users`
+		SET average_score = @avg_score
+		WHERE id = user_id;
+	END IF;
+    END;//
+
+DELIMITER ;
